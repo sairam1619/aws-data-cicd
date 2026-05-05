f6294622dd06b2354e86364c6ff09cc05f7a1756
sairam1619 <sairamgangapuram2001@gmail.com>
2026-05-05 23:28:23 +0530

    man now will only create a lambda function

--- main.tf
+++ main.tf
@@ -1,44 +1,3 @@
-resource "aws_s3_bucket" "bucket" {
-  bucket = var.bucket_name
-}
-
-resource "aws_dynamodb_table" "lock_table" {
-  name         = "lock_table"
-  billing_mode = "PAY_PER_REQUEST"
-
-  # Primary Key
-  hash_key  = "UserID"
-  range_key = "OrderID"
-
-  attribute {
-    name = "UserID"
-    type = "S"
-  }
-
-  attribute {
-    name = "OrderID"
-    type = "S"
-  }
-
-  attribute {
-    name = "Email"
-    type = "S"
-  }
-
-  attribute {
-    name = "CreatedAt"
-    type = "S"
-  }
-
-  # Global Secondary Index (uses remaining attributes)
-  global_secondary_index {
-    name            = "EmailIndex"
-    hash_key        = "Email"
-    range_key       = "CreatedAt"
-    projection_type = "ALL"
-  }
-}
-
 resource "aws_lambda_function" "lambda" {
   function_name = "demo_lambda"
   role          = "arn:aws:iam::854359848205:role/athena_lambda_iam"
@@ -53,4 +12,3 @@ data "aws_caller_identity" "current" {}
 locals {
   account_id = data.aws_caller_identity.current.account_id
 }
-
